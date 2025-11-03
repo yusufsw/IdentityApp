ec408a05b2943cae4fb246066dd32a8b4c364a57
yusufsw <yusuf-talha@outlook.com>
2025-11-03 07:40:08 +0300

    Updated Roles/Edit.cshtml and RolesController.cs

---
+++ Migrations/20251103003733_removeColumnsOfAppRole.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class removeColumnsOfAppRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FullName",
+                table: "AspNetRoles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FullName",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FullName",
+                table: "AspNetRoles",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ Models/IdentitySeedData.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Models
+{
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "admin";
+        private const string adminPassword = "Admin_12345";
+
+        public static async void IdentityTestUser(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IdentityContext>();
+
+            if (context.Database.GetAppliedMigrations().Any())
+            {
+                context.Database.Migrate(); //database update komutuna karsilik gelir
+            }
+
+            var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<AppUser>>();
+
+            var user = await userManager.FindByNameAsync(adminUser);
+
+            if (user == null)
+            {
+                user = new AppUser
+                {
+                    FullName = "Yusuf Talha ERDOGMUS",
+                    UserName = adminUser,
+                    Email = "admin@yusuf.com",
+                    PhoneNumber = "551"
+
+                };
+
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+    }
+}
