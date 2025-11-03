5d9e7051e0d2a74d2cc1baa3f44b456050fa146e
yusufsw <yusuf-talha@outlook.com>
2025-11-04 00:28:26 +0300

    added SignManager to AccountController, for cookies

--- obj/Debug/net7.0/IdentityApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/IdentityApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91dc370a83b0fcc166f62478b0f069301c604c0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+080527ee9bf2b7701b0845993cfec19dec3bc3a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
